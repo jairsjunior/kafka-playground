c5f32fbf49c9cc5d39a13a0dc8eff9ced7bad726
Jair de Souza Junior <detento112@gmail.com>
2018-12-21 10:25:51 -0200

    Changes to have a quick-setup

---
+++ kafka-using-node/src/consumer/checkIsUp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#Check if server is up
+
+while [[ "$(nc -z kafka 9092 </dev/null; echo $?)" !=  "0" ]]; 
+do sleep 5;
+echo "Waiting for KAFKA SERVER is UP and RESPONDING"; 
+done
+
+sleep 15;
+
+npm start
